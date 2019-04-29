96bbf74c159998a33ad8870b1053233efe5dd56f
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-04-29 19:57:20 -0400

    ItemPage
    Created the ItemPage component which uses map to iterate through an arrays of objects to return each objects name, also created a proptype to make sure that the ItemPage Component only accepts an array as a prop

--- src/Item-page.js
+++ src/Item-page.js
@@ -1,3 +1,18 @@
 import React from "react";
-import PropTypes from "prop-types";
+import PropTypes, { array } from "prop-types";
 import "./ItemPage.css";
+
+var ItemPage = ({ items }) => {
+  return (
+    <ul className="ItemPage-items">
+      {items.map(item => (
+        <li key={item.id} className="ItemPage-item">
+          {item.name}
+        </li>
+      ))}
+    </ul>
+  );
+};
+ItemPage.propTypes = {
+  items: PropTypes.array.isRequired
+};
