4ebbaad79219e932207c5b34728e98cbd358878f
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-04-30 12:02:03 -0400

    Item Component
    Set up Item Component so that it should accept an array prop, and a function

--- src/item.js
+++ src/item.js
@@ -1,3 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./item.css";
+import "./Item.css";
+
+const Item = ({ item, onAddToCart }) => {
+  return (
+    <div className="item">
+      <div className="itemLeft">
+        <div className="itemImage" />
+        <div className="itemTitle">{item.name}</div>
+        <div className="itemDescription">{item.description}</div>
+      </div>
+
+      <div className="itemRight">
+        <div className="itemPrice">${item.price}</div>
+        <button className="itemAddToCart" onClick={onAddToCart}>
+          Add To Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+Item.propTypes = {
+  item: PropTypes.object.isRequired,
+  onAddToCart: PropTypes.func.isRequired
+};
