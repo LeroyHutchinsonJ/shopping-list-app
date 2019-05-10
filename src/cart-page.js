26cb2a4d9b98c87ea11d60439fe8e12f821022fc
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-05-10 12:07:03 -0400

    Color Change
    Changed the color of the add item prompt

--- src/cart-page.js
+++ src/cart-page.js
@@ -5,12 +5,18 @@ import Item from "./item";
 
 var CartPage = ({ items, onAddOne, onRemoveOne, itemPrice }) => {
   var showWords = "emptyCart";
+  var showWordsTwo = "emptyCartTwo";
   var hideWords = "";
 
   return (
     <>
       <ul className="cartPageItems">
-        <div className={items.length === 0 ? showWords : hideWords} />
+        <div className="addItemsPrompt">
+          {" "}
+          <div className={items.length === 0 ? showWords : hideWords} />
+          <div className={items.length === 0 ? showWordsTwo : hideWords} />
+        </div>
+
         {items.map(item => (
           <li key={item.id} className="cartPageItem">
             <Item item={item}>
